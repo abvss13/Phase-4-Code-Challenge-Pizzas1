e92138c940a5fba2bc0f245035c80aeed6aab2f0
Ahmed yussuf Abdullahi <ahmedyussuf917@gmail.com>
2024-01-24 10:26:03 +0300

    updated client

--- client/src/components/Restaurant.js
+++ client/src/components/Restaurant.js
@@ -1,39 +1,66 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import PizzaForm from "./PizzaForm";
 
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-
-export default function Restaurant() {
-  const [restaurant, setRestaurant] = useState([]);
-
-  const url = "http://127.0.0.1:8000/restaurants";
-
-  const fetchRestaurant = async () => {
-    try {
-      const response = await fetch(url);
-      if (response.ok) {
-        const data = await response.json();
-        setRestaurant(data);
-      } else {
-        throw new Error("Something went wrong");
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
+function Home() {
+  const [{ data: restaurant, error, status }, setRestaurant] = useState({
+    data: null,
+    error: null,
+    status: "pending",
+  });
+  const { id } = useParams();
 
   useEffect(() => {
-    fetchRestaurant();
-  }, []);
+    fetch(`/restaurants/${id}`).then((r) => {
+      if (r.ok) {
+        r.json().then((restaurant) =>
+          setRestaurant({ data: restaurant, error: null, status: "resolved" })
+        );
+      } else {
+        r.json().then((err) =>
+          setRestaurant({ data: null, error: err.error, status: "rejected" })
+        );
+      }
+    });
+  }, [id]);
 
-  console.log(restaurant);
+  function handleAddPizza(newPizza) {
+    setRestaurant({
+      data: {
+        ...restaurant,
+        pizzas: [...restaurant.pizzas, newPizza],
+      },
+      error: null,
+      status: "resolved",
+    });
+  }
+
+  if (status === "pending") return <h1>Loading...</h1>;
+  if (status === "rejected") return <h1>Error: {error.error}</h1>;
 
   return (
-    <div>
-        {restaurant?.map(rest => (
-            <ul key={rest.id}>
-                <Link to={'/rest'} >{rest.name}</Link>
-            </ul>
+    <section className="container">
+      <div className="card">
+        <h1>{restaurant.name}</h1>
+        <p>{restaurant.address}</p>
+      </div>
+      <div className="card">
+        <h2>Pizza Menu</h2>
+        {restaurant.pizzas.map((pizza) => (
+          <div key={pizza.id}>
+            <h3>{pizza.name}</h3>
+            <p>
+              <em>{pizza.ingredients}</em>
+            </p>
+          </div>
         ))}
-    </div>
+      </div>
+      <div className="card">
+        <h3>Add New Pizza</h3>
+        <PizzaForm restaurantId={restaurant.id} onAddPizza={handleAddPizza} />
+      </div>
+    </section>
   );
 }
+
+export default Home;
